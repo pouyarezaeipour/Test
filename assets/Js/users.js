2d6ca4c9bf505155544803867629cca4251917b0
pouya <puyarezaeiporaza@gmail.com>
2023-09-03 16:10:05 +0330

    design our page

--- assets/Js/users.js
+++ assets/Js/users.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
+
+
+
+    const adduserbtn = document.getElementById("ADDBTN");
+    adduserbtn.addEventListener("click",()=>{
+      document.querySelector(".add_user_model").classList.add("show");
+      document.querySelector(".add_user_model_back").classList.remove("dis-none");
+    });
+
+    document.querySelector(".add_user_model_back").addEventListener("click",(e)=>{
+      document.querySelector(".add_user_model").classList.remove("show");
+      document.querySelector(".add_user_model_back").classList.add("dis-none");
+
+    });
   });
