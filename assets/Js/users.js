bd69c6923742656bdf24a9b8c5bd348205771817
pouya <puyarezaeiporaza@gmail.com>
2023-09-20 16:04:56 +0330

    slash and exercize

--- assets/Js/users.js
+++ assets/Js/users.js
@@ -14,5 +14,62 @@ document.addEventListener('DOMContentLoaded', function() {
       document.querySelector(".add_user_model").classList.remove("show");
       document.querySelector(".add_user_model_back").classList.add("dis-none");
 
+
+
+      
+
+
     });
+
+
+    
+
+
+//     fetch('https://jsonplaceholder.typicode.com/users', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     email: 'pouya@gmail.com',
+//     id : 1 ,
+//     name : "pouya",
+//     phone: "545454",
+//     username : "pou",
+//     website : "pou.com"
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then((response) => response.json())
+// .then((json) => {
+//   console.log(json);
+// html = "";
+// for(const user of json){
+//     html = html + `   <tr>
+//     <td>${user.id}</td>
+//     <td>${user.name}</td>
+//     <td>${user.username}</td>
+//     <td>p${user.email}</td>
+//     <td>${user.address.city}</td>
+//     <td>
+//       <span>action :</span>
+//       <i class="material-icons p-l-5 p-r-2 yellow-text">edit</i>
+//       <i class="material-icons p-l-5 p-r-2  orange-text">delete</i>
+//     </td>
+//   </tr>`
+// }
+
+// document.querySelector(".body-of-table").innerHTML=html;
+
+
+// });
+
+
+
   });
+
+
+  
